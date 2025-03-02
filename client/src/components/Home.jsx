f249fb95b9fc28fb8ff8b9efb4fd4169344d41f6
biscake <leonut.ljh@gmail.com>
2025-03-02 23:52:42 +0800

    Added: logout button

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,14 +1,33 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const [text, setText] = useState();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    axios.post('http://localhost:3000/api/users/logout')
+      .then(res => {
+        console.log(res);
+        return navigate('/');
+      })
+      .catch(err => {
+        console.log(err)
+      });
+  }
+
   useEffect(() => {
-    axios.get('http://localhost:3000/api/posts', { withCredentials: true })
-      .then(res => console.log(res))
-      .catch(err => console.log(err))
-  })
+    axios.get('http://localhost:3000/api/posts')
+      .then(res => setText('authorized'))
+      .catch(err => setText('unauthorized'))
+  }, [])
+
   return (
-    <p>Temp</p>
+    <>
+      <p>{text}</p>
+      <button onClick={handleLogout}>log out</button>
+    </>
   )
 }
 
--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,15 +1,36 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styles from '../assets/Home.module.css';
 import LoginForm from "./forms/LoginForm";
 import SignupForm from "./forms/SignupForm";
 
 const Welcome = () => {
   const [isLoginForm, setIsLoginForm] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/api/users/verify-session")
+      .then(res => {
+        return navigate("/home");
+      })
+      .finally(() => {
+        setIsLoading(false);
+      })
+      setIsLoading(false);
+  }, [])
+
+  // TODO: add loading component
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
 
   return (
     <div className={styles.wrapper}>
       <div className={styles.main}>
-        <div className={styles.welcome}>Insta</div>
+        <div className={styles.welcome}>Blab</div>
         {isLoginForm? <LoginForm/> : <SignupForm/>}
       </div>
       <div className={styles['sign-up']}>
@@ -17,7 +38,7 @@ const Welcome = () => {
         <button className={styles.button} onClick={() => setIsLoginForm(!isLoginForm)}>{isLoginForm ? 'Register' : 'Log in'}</button>
       </div>
     </div>
-  )
+  )   
 }
 
 export default Welcome;
